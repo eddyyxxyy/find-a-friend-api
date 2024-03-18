9acafadcb7f1b10ab2154c1a959dbb88341c2a0f
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2024-03-18 15:55:57 -0300

    test(services): :white_check_mark: checks if Pet creation works as app and business rules imply

---
+++ src/services/create-pet.spec.ts
@@ -0,0 +1,72 @@
+import { makeOrg } from "@tests/make-org";
+import { makePet } from "@tests/make-pet";
+import { hash } from "bcrypt";
+import { beforeEach, describe, expect, it } from "vitest";
+
+import {
+  InMemoryOrgsRepository,
+} from "@/repositories/in-memory/orgs.repository";
+import {
+  InMemoryPetsRepository,
+} from "@/repositories/in-memory/pets.repository";
+
+import { CreatePetService } from "./create-pet";
+import { OrgDoesntExistError } from "./errors/org-doesnt-exist-error";
+
+let inMemoryOrgsRepository: InMemoryOrgsRepository;
+let inMemoryPetsRepository: InMemoryPetsRepository;
+let sut: CreatePetService;
+
+describe("Create Pet Service", () => {
+  beforeEach(() => {
+    inMemoryOrgsRepository = new InMemoryOrgsRepository();
+    inMemoryPetsRepository = new InMemoryPetsRepository();
+    sut = new CreatePetService(inMemoryOrgsRepository, inMemoryPetsRepository);
+  });
+
+  it("should be able to create a pet", async () => {
+    const orgTobeCreate = makeOrg();
+
+    const { id: orgId } = await inMemoryOrgsRepository.create({
+      ...orgTobeCreate,
+      passwordHash: await hash(
+        orgTobeCreate.password,
+        10,
+      ),
+    });
+
+    const petToBeCreated = makePet({ orgId });
+
+    const { pet: createdPet } = await sut.execute({
+      data: {
+        about: petToBeCreated.about,
+        age: petToBeCreated.age,
+        energyLevel: petToBeCreated.energyLevel,
+        environmentMinimumSize: petToBeCreated.environmentMinimumSize,
+        independenceLevel: petToBeCreated.independenceLevel,
+        name: petToBeCreated.name,
+        orgId,
+        size: petToBeCreated.size,
+      },
+    });
+
+    expect(createdPet.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to create a pet with invalid org id", async () => {
+    const petToBeCreated = makePet();
+
+    await expect(() => sut.execute({
+      data: {
+        about: petToBeCreated.about,
+        age: petToBeCreated.age,
+        energyLevel: petToBeCreated.energyLevel,
+        environmentMinimumSize: petToBeCreated.environmentMinimumSize,
+        independenceLevel: petToBeCreated.independenceLevel,
+        name: petToBeCreated.name,
+        orgId: "invalid-org-id",
+        size: petToBeCreated.size,
+      },
+    })).rejects.toBeInstanceOf(OrgDoesntExistError);
+  });
+});
---
+++ tests/make-pet.ts
@@ -0,0 +1,52 @@
+import { randomUUID } from 'node:crypto';
+import { faker } from '@faker-js/faker';
+
+type TSizes = "SMALL" | "MEDIUM" | "LARGE" | "HUGE";
+type TEnergyAndIndependenceLevels = "LOW" | "MEDIUM" | "HIGH";
+type TEnvironmentMinimumSize = "SMALL" | "MEDIUM" | "WIDE" | "XWIDE";
+
+interface Pet {
+  id: string;
+  name: string;
+  about: string;
+  age: number;
+  size: TSizes;
+  energyLevel: TEnergyAndIndependenceLevels;
+  independenceLevel: TEnergyAndIndependenceLevels;
+  environmentMinimumSize: TEnvironmentMinimumSize;
+  wasAdopted: boolean;
+  orgId: string | null;
+}
+
+type Overwrite = {
+  age?: number;
+  energyLevel?: TEnergyAndIndependenceLevels;
+  environmentMinimumSize?: TEnvironmentMinimumSize;
+  independenceLevel?: TEnergyAndIndependenceLevels;
+  orgId?: string;
+  size?: TSizes;
+}
+
+/**
+* Creates an pet object with default values for testing purposes
+*
+* @returns {Pet} An `Pet` object
+*/
+function makePet(
+  overwrite?: Overwrite
+): Pet {
+  return {
+    id: randomUUID(),
+    about: faker.lorem.paragraph(),
+    age: overwrite?.age ?? 2,
+    energyLevel: overwrite?.energyLevel ?? faker.helpers.arrayElement(["LOW", "MEDIUM", "HIGH"]),
+    environmentMinimumSize: overwrite?.environmentMinimumSize ?? faker.helpers.arrayElement(["SMALL", "MEDIUM", "WIDE", "XWIDE"]),
+    independenceLevel: overwrite?.independenceLevel ?? faker.helpers.arrayElement(["LOW", "MEDIUM", "HIGH"]),
+    name: faker.animal.dog(),
+    orgId: overwrite?.orgId ?? randomUUID(),
+    size: overwrite?.size ?? faker.helpers.arrayElement(["SMALL", "MEDIUM", "LARGE", "HUGE"]),
+    wasAdopted: false,
+  };
+}
+
+export { makePet };
